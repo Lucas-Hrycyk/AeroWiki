04ae617b3da626d92a860e6fb057491d13e4be1b
Lucas Antonio Alves Hrycyk <lucasantonio_02@hotmail.com>
2024-04-23 22:15:22 -0300

    Primeiro Commit

---
+++ Script/Index.js
@@ -0,0 +1,12 @@
+document.getElementById('toggleNavButton').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('hidden');
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuDiv = document.getElementById('menu');
+    menuDiv.classList.add('hidden');
+});
+
+    
