b9e7b6d3f5aff2eadb8d359cddd0fec0b8c05156
Lucas Antonio Alves Hrycyk <lucas.alves@almeidakruger.com.br>
2024-04-29 17:24:46 -0300

    Tentativa de Separar MENU

--- Script/Index.js
+++ Script/Index.js
@@ -1,16 +1,20 @@
-document.getElementById('toggleNavButton').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('hidden');
-});
-
-
 document.addEventListener('DOMContentLoaded', function() {
     var menuDiv = document.getElementById('menu');
     menuDiv.classList.add('hidden');
-});
-
-
-document.getElementById('toggleNavButtonPhone').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('hidden');
-});
+    
+    document.getElementById('toggleNavButton').addEventListener('click', function() {
+      var menu = document.getElementById('menu');
+      menu.classList.toggle('hidden');
+    });
+  
+    fetch('menu.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('menu').innerHTML = data;
+  
+        document.getElementById('toggleNavButtonPhone').addEventListener('click', function() {
+          var menu = document.getElementById('menu');
+          menu.classList.toggle('hidden');
+        });
+      });
+  });
